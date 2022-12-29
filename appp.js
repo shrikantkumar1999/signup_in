b05f2afd59890396692058e09f7f4419c097eeaf
shrikantkumar1999 <60041158+shrikantkumar1999@users.noreply.github.com>
2022-12-29 16:02:31 +0530

    Update appp.js

--- appp.js
+++ appp.js
@@ -12,7 +12,7 @@
 var firestore= firebase.firestore();
 const db= firestore.collection("hack3");
 function myFunction() {
-  location.replace("/Signin.html");
+  location.replace("/signup_in/Signin.html");
 }
 let submitButton =document.getElementById('submit');
 submitButton.addEventListener("click",(e) => {
